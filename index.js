a5023bc3bfb29da1c78406185aca42303e790fa0
Airsane <Airsane@gmail.com>
2025-08-11 19:06:24 +0200

    Add logging functionality and user status management
    - Integrated pino for structured logging.
    - Implemented user status checking every 5 minutes based on environment configuration.
    - Updated message handling to log incoming and outgoing messages.
    - Enhanced bot initialization with logger setup.
    - Added pino-loki and pino-pretty as dependencies for logging.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Discord js bot
+const pino = require('pino');
 
 const { Client } = require('discord.js-selfbot-v13');
 const client = new Client();
@@ -8,10 +9,34 @@ dotenv.config();
 const token = process.env.TOKEN;
 const guildId = process.env.GUILD_ID;
 const pingedUserId = process.env.PINGED_USER_ID;
+const checkStatus = process.env.CHECK_STATUS.toLowerCase() === 'true';
+let logger;
 
-client.once('ready', () => {
-    console.log(`Logged in as ${client.user.tag}`);
+client.once('ready', async () => {
+    logger.info(`Logged in as ${client.user.tag}`);
+    // check user status every 5 minuts
+    if(checkStatus){
+        setInterval(checkUserStatus, 60 * 1000 * 5);
+    }
 });
+
+const checkUserStatus = async () =>{
+    const guild = client.guilds.cache.get(guildId);
+    const user = await guild.members.fetch(pingedUserId);
+    const presence = user.presence?.status;
+    const currentStatus = client.user.presence?.status;
+    logger.info(`Current status: ${currentStatus}, Presence: ${presence}`);
+    if(presence === currentStatus) return;
+    if(presence === undefined && currentStatus === 'invisible') return;
+
+    if(presence !== undefined){
+        client.user.setStatus(presence);
+        logger.info('Setting status to ' + presence);
+    } else{
+        client.user.setStatus('invisible');
+        logger.info('Setting status to invisible');
+    }
+}
 
 const handleIncomingMessage = async (message) => {
     if (message.author.bot) return;
@@ -25,8 +50,10 @@ const handleIncomingMessage = async (message) => {
             message.author.id,
             { type: 'GUILD_TEXT' }
         );
-        console.log(`Created channel ${channel.name}`);
+        logger.info(`Created channel ${channel.name}`);
     }
+
+    logger.info(`Received message from ${message.author.username} in ${channel.name}`);
 
     // Prepare message options
     let content = `<@${pingedUserId}> ${message.author.username}:`;
@@ -55,6 +82,8 @@ const handleOutgoingMessage = async (message) => {
     const user = client.users.cache.get(userId);
     if (!user) return;
 
+    logger.info(`Sending message to ${user.username}`);
+
     // Prepare message options
     let options = {};
     if (message.attachments && message.attachments.size > 0) {
@@ -67,10 +96,32 @@ const handleOutgoingMessage = async (message) => {
     }
 }
 
-
 client.on('message', async (message) => {
     handleIncomingMessage(message);
     handleOutgoingMessage(message);
 });
 
+const transport = pino.transport({
+    targets:[
+        {
+            target: 'pino-loki',
+            options:{
+                host: process.env.LOKI_HOST,
+                labels:{app: 'whisper-bot', env:"production"},
+                batching:true,
+                interval: 5,
+            }
+        },
+        {
+            target: 'pino-pretty',
+            options:{
+                colorize: true,
+                singleLine: true,
+                ignore: 'pid,hostname',
+            }
+        }
+    ]
+});
+logger = pino(transport);
+logger.info('Logger initialized, starting bot login...');
 client.login(token);
